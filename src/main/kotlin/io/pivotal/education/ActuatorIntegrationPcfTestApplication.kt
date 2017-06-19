ce12c2b66e31f483d463bb2bdeb0ddb4b94b9c93
Eitan Suez <eitan.suez@gmail.com>
2017-06-19 18:47:41 -0500

    moved class into same file as application class

--- src/main/kotlin/io/pivotal/education/ActuatorIntegrationPcfTestApplication.kt
+++ src/main/kotlin/io/pivotal/education/ActuatorIntegrationPcfTestApplication.kt
@@ -2,10 +2,21 @@ package io.pivotal.education
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class ActuatorIntegrationPcfTestApplication
 
 fun main(args: Array<String>) {
-    SpringApplication.run(ActuatorIntegrationPcfTestApplication::class.java, *args)
+  SpringApplication.run(ActuatorIntegrationPcfTestApplication::class.java, *args)
+}
+
+
+@RestController
+class HelloController {
+
+  @GetMapping("/")
+  fun hello(): String = "hello world"
+
 }
